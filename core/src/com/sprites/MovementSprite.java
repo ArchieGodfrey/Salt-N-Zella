a08ce6a13453f68d6e7c0b6041dbfdc53a75e4c8
Archie Godfrey <archiegod@gmail.com>
2020-03-06 11:58:39 +0000

    Finally fixed collisions

--- core/src/com/sprites/MovementSprite.java
+++ core/src/com/sprites/MovementSprite.java
@@ -115,7 +115,7 @@ public class MovementSprite extends SimpleSprite {
 
     /*
      *  =======================================================================
-     *                          Modified for Assessment 3
+     *           Modified for Assessment 4     @author Archie Godfrey
      *  =======================================================================
      */
     /**
@@ -124,13 +124,13 @@ public class MovementSprite extends SimpleSprite {
      */
     private void accelerate() {
         // Calculate whether it hits any boundaries
-        int collisions = collidesWithBlockedTile(this.collisionLayer);
-        // Check if it collides with any tiles, then move the sprite
-        if (collisions == 0) {
+        int vertices = verticesOnRoad(this.collisionLayer);
+        // If the sprite should collide, check that all 3 vertices are on a road, then move the sprite
+        if (this.collisionLayer == null || vertices == getPolygonVertices(super.getMovementHitBox()).size) {
             this.setX(this.getX() + this.speed.x * Gdx.graphics.getDeltaTime());
             this.setY(this.getY() + this.speed.y * Gdx.graphics.getDeltaTime());
             if (this.decelerationRate != 0) decelerate();
-        } else if (collisions == 1){
+        } else if (vertices > 0){
             // Separate the sprite from the tile and stop sprite movement
             if (Math.abs(this.speed.x) > Math.abs(this.speed.y)) {
                 this.speed = new Vector2(this.speed.x*.85f, -this.speed.y*.75f);
@@ -162,23 +162,23 @@ public class MovementSprite extends SimpleSprite {
 
     /*
      *  =======================================================================
-     *                          Modified for Assessment 3
+     *           Modified for Assessment 4     @author Archie Godfrey
      *  =======================================================================
      */
     /**
-     * Checks if the tile at a location is a "blocked" tile or not.
-     * @return Whether the hits a collision object (true) or not (false)
+     * Checks if the tile at a location is a "road" tile or not.
+     * @return Whether the sprite is on a road (true) or not (false)
      */
-    private int collidesWithBlockedTile(TiledMapTileLayer layer) {
-        int collisions = 0;
+    private int verticesOnRoad(TiledMapTileLayer layer) {
+        int vertices = 0;
         if (layer != null) {
             for (Vector2 vertex : getPolygonVertices(super.getMovementHitBox())) {
                 if (layer.getCell(((int) (vertex.x / TILE_DIMS)), ((int) (vertex.y / TILE_DIMS))) != null) {
-                    collisions++;
+                    vertices++;
                 }
             }
         }
-        return collisions;
+        return vertices;
     }
 
     /*
